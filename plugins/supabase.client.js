c0074b4c5d14915722d0434193e848ed7af7c4c7
keshu003 <155677314+keshu003@users.noreply.github.com>
2025-08-08 18:04:12 +0530

    Fix: Remove hardcoded Supabase key

--- plugins/supabase.client.js
+++ plugins/supabase.client.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 export default defineNuxtPlugin(() => {
-  const supabaseUrl = process.env.SUPABASE_URL || 'https://bruqgipiahcrpelverjr.supabase.co'
-  const supabaseKey = process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJydXFnaXBpYWhjcnBlbHZlcmpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1ODEyNTQsImV4cCI6MjA3MDE1NzI1NH0.0s5mowqQtd5Tv7UM9IdUCqKJGvLwVKUR94Xosp1KoLQ'
+  const supabaseUrl = process.env.SUPABASE_URL
+  const supabaseKey = process.env.SUPABASE_KEY
   const supabase = createClient(supabaseUrl, supabaseKey)
 
   return {
