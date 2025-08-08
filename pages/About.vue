5a2040e4720cb80f42a0d4cd6661df0bb95431fb
keshu003 <155677314+keshu003@users.noreply.github.com>
2025-08-08 15:01:05 +0530

    modifiedfiles

--- pages/About.vue
+++ pages/About.vue
@@ -1,7 +1,7 @@
-<template>
+<!-- <template>
     <div class="min-h-screen bg-gray-900 text-white px-6 md:px-16 py-10">
         <div>
-            
+    hello
         </div>
     </div>
 </template>
@@ -12,4 +12,4 @@
 
 <style lang="scss" scoped>
 
-</style>
+</style> -->
